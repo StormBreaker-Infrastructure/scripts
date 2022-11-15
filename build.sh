c7d9e032c3120fe0501fb9418f23941f69c383ee
danascape <danascape@gmail.com>
2022-11-16 00:42:44 +0530

    CI: Exit if no arguments in input

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@
 # Copyright (C) 2020 Saalim Quadri (iamsaalim)
 # SPDX-License-Identifier: Apache-2.0 license
 
+# Exit is nothing is set
+[[ $# = 0 ]] && echo "No Device Input" && exit 1
+
 # Set Variables
 DEVICE="$1"
 
 commit-id() {
     echo "Checking commit-id of $DEVICE"
-
 }
 
 commit-id
