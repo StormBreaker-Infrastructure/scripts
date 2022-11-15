0c2769a7f8af49f725b62875ae70276df15aa999
danascape <danascape@gmail.com>
2022-11-16 00:52:19 +0530

    CI: Add a placeholder for comparing the commit id

--- build.sh
+++ build.sh
@@ -12,12 +12,17 @@
 # Set Variables
 DEVICE="$1"
 
-commit-id() {
+fetch-commit-id() {
     echo "Checking commit-id of $DEVICE"
     echo "Fetching remote information of the device"
     COMMIT_ID=$(git ls-remote https://github.com/stormbreaker-project/$DEVICE | head -1 | cut -f -1)
     echo $COMMIT_ID
+    compare-commit-id
     echo "$COMMIT_ID" >> commit-id/$DEVICE-id
 }
 
-commit-id
+compare-commit-id() {
+    echo "hello"
+}
+
+fetch-commit-id
