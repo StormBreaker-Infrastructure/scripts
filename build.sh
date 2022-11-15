b9422bada94c4ce6832afb7761bd017298e78649
danascape <danascape@gmail.com>
2022-11-16 00:44:00 +0530

    CI: Update copyright
    * Forgot

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2020 Saalim Quadri (iamsaalim)
+#
+# Copyright (C) 2019 Saalim Quadri (danascape)
+#
 # SPDX-License-Identifier: Apache-2.0 license
+#
 
 # Exit is nothing is set
 [[ $# = 0 ]] && echo "No Device Input" && exit 1
