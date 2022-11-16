2813a833a26f2994bac452725ece82372be56a51
danascape <danascape@gmail.com>
2022-11-16 23:33:13 +0530

    CI: Wrap kernel build under a function

--- build.sh
+++ build.sh
@@ -137,15 +137,19 @@ makeDefconfig() {
     fi
 }
 
+makeKernel() {
+    if [[ "$VERSION" == "3.18" ]]; then
+        make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+    fi
+}
+
 triggerBuild() {
     cloneCompiler
     echo "Starting Build"
     echo "Using config $DEVICE_CONFIG"
     cd $BUILD_DIR
     makeDefconfig
-    if [[ "$VERSION" == "3.18" ]]; then
-        make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
-    fi
+    makeKernel
 }
 
 fetch-commit-id
